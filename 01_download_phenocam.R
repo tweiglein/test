54043a3780c2b745623a58f70c0a5e2e11dfedab
Tyler Weiglein <tweiglein@Red-Cedar.local>
2022-02-23 13:59:05 -0500

    Test

---
+++ 01_download_phenocam.R
@@ -0,0 +1,18 @@
+##' Download PhenoCam data
+##' 
+##' @param URL  web address where data is located
+download_phenocam <- function(URL) {
+  ## check that we've been passed a URL
+  if (length(URL) == 1 & is.character(URL) & substr(URL, 1, 4) == "http") {
+    
+    ## read data
+    dat <- read_csv(URL, skip = 22)
+    
+    ## convert date
+    dat$date <- as_date(as.character(dat$date))
+    
+    return(dat)
+  } else {
+    print(paste("download.phenocam: Input URL not provided correctly", URL))
+  }
+}
